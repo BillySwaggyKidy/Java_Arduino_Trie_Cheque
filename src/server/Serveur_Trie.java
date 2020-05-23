1ddc426152951a54835cca06b542942a3ef40876
Mathias Rando <mathias.rando13@gmail.com>
2020-05-23 17:09:12 +0200

    partie serveur + utilisation librairie
    + ajout rep lib avec la lib JSerialcomm
    + ajout class Thread_serveur contenant algo + envoie vers arduino
    + modif classe Serveur_Trie
    - suppression lib du rep server

--- src/server/Serveur_Trie.java
+++ src/server/Serveur_Trie.java
@@ -1,7 +1,37 @@
 package server;
 
+import java.util.*;
+import java.io.*;
+import java.net.*; // permet de pouvoir faire du réseau avec java
+
 public class Serveur_Trie 
 {
     private static int num_port;
     private static boolean is_running = true;
+
+
+
+    public static void main(String[] args) throws IOException // a executer dans un autre ordinateur
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenue au serveur, Veillez saisir...");
+        System.out.println("Votre numéro de port >");
+        num_port = scanner.nextInt();
+
+        try (
+        ServerSocket serverSocket = new ServerSocket(num_port); // on crée l'application serveur sur un port
+        )
+        {
+            while (is_running == true)
+            {
+                Thread_serveur thread_serveur = new Thread_serveur(serverSocket.accept(), tableau_nom); // on attend et accepte la demande de connexion d'un client
+                thread_serveur.start();
+            }
+        }
+        catch (IOException e)
+        {
+
+        }
+
+    }
 }
---
+++ src/server/Thread_serveur.java
@@ -0,0 +1,70 @@
+package server;
+
+import java.util.*;
+import java.io.*;
+import java.net.*; // permet de pouvoir faire du réseau avec java
+
+import com.fazecast.jSerialComm.*;
+
+public class Thread_serveur extends Thread
+{
+    private int num_cheque; // TEMPORAIRE: pour tester l'envoie de donnee du client -> serveur -> arduino
+    private ObjectOutputStream out_thread; // on crée un objet capable de contenir un message que on va envoyer au serveur
+    private ObjectInputStream in_thread; //  on crée un objet capable de contenir la réponse du serveur
+
+    public Thread_serveur(int num)
+    {
+        num_cheque = num;
+    }
+
+
+    public int algorithme_trie(int num_donnee) // permet de trouver la case pour trier le cheque
+    {
+        return num_donnee;
+    }
+
+
+
+    public void envoie_donnee_arduino(int num_led) // permet d'envoyer les données de l'algorithme à la partie Arduino
+    {
+        SerialPort sp = SerialPort.getCommPort("/dev/ttyACM1");
+
+        sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
+        sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
+    
+        if (sp.openPort()) //ouvre le port en question en mode lecture et écriture
+        {
+            System.out.println("Port is open");
+        } 
+        else 
+        {
+            System.out.println("Failed to open port");
+            return;
+        }
+        
+        for (Integer i = 0; i < 5; ++i) 
+        {            
+            sp.getOutputStream().write(i.byteValue()); // écrit les données en BYTE du serveur vers le port
+            sp.getOutputStream().flush(); // permet de forcet l'écriture des donnés du buffer
+            Thread.sleep(1000);
+        }        
+          
+        if (sp.closePort()) //ferme le port en question en mode lecture et écriture
+        {
+            System.out.println("Port is closed :)");
+        } 
+        else 
+        {
+            System.out.println("Failed to close port :(");
+            return;
+          
+        }
+
+    }
+
+    public void run()
+    {
+        envoie_donnee_arduino(algorithme_trie(num_cheque));
+    }
+
+}
